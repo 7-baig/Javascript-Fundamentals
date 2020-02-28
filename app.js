17eb6aca47440f3d55f55cc453512105e6ba9d45
7-baig <saadbaig7777@gmail.com>
2020-02-27 23:57:11 -0800

    changed app.js

---
+++ app.js
@@ -0,0 +1,269 @@
+// function foo(x) {
+//   if (x < 1) {
+//     return "Less than one";
+//   } else if (x < 2) {
+//     return "Less than two";
+//   } else {
+//     return "Greater than or equal to two";
+//   }
+// }
+// var res = foo(3);
+// console.log(res);
+// =======*********************************
+// function prac(a, b) {
+//   return a === b;
+// }
+// var result = prac(5, 5);
+// console.log(result);
+
+// function phoneticLookup(val) {
+//   var result = "";
+//   var lookup = {
+//     "alpha": "Adams",
+//     "bravo": "Boston",
+//     "charlie": "Chicago",
+//     "delta": "Denver",
+//     "echo": "Easy",
+//     "foxtrot": "Frank",  
+// }
+// result = lookup[val];
+// return result;
+// }
+// var hi = phoneticLookup("charlie");
+// console.log(hi);
+// ************************
+// var myObj = {
+//   gift: "pony",
+//   pet: "kitten",
+//   bed: "sleigh"
+// };
+
+// function checkObj(checkProp) {
+//   var a = myObj.hasOwnProperty(checkProp);
+//   return a;
+// }
+
+// console.log(checkObj("house"));
+// checkObj("kitten");
+// checkObj("house");
+
+// *************************************
+//  ***********************  OBJECTS **********************
+//   var collection = {
+//     2548: {
+//       album: "Slippery When Wet",
+//       artist: "Bon Jovi",
+//       tracks: [
+//         "Let It Rock",
+//         "You Give Love a Bad Name"
+//       ]
+//     },
+//     2468: {
+//       album: "1999",
+//       artist: "Prince",
+//       tracks: [
+//         "1999",
+//         "Little Red Corvette"
+//       ]
+//     },
+//     1245: {
+//       artist: "Robert Palmer",
+//       tracks: [ ]
+//     },
+//     5439: {
+//       album: "ABBA Gold"
+//     }
+//   };
+// function updateRecords(id, prop, value) {
+//   if(value === "") {
+//     delete collection[id][prop]
+//   }
+//   else if (prop === "tracks") {
+//     collection[id][prop] = collection[id][prop] || [];
+//     collection[id][prop].push(value)
+//   }
+//   else {
+//     collection[id][prop] = value;
+//   }
+// }
+// var res = updateRecords(2468, "tracks", "hello");
+// console.log(res);
+
+// ************** OBJECTS (MY VERSION) ****************
+
+// var bio = {
+//   name: "Saad",
+//   age: 19,
+//   nationality: "Pakistani",
+//   "favourite food": "Biryani",
+//   passion: "coding",
+//   skills: [
+//     "HTML",
+//     "CSS",
+//     "Javascript",
+//     "Kuberenetes",
+//     "Docker"
+//   ],
+//   vehicles: {
+//     car: "Civic",
+//     bike: "yamaha",
+//     plane: "lufthansa"
+//   }
+// }
+// var secondPlane = bio.vehicles["bike"];
+// console.log(secondPlane);
+// var push = bio.skills.push("React");
+// console.log(bio);
+
+// TABLE ***********
+
+// var array = [];
+// var i = 2;
+// var j = 1
+// while(i <= 20) {
+//     array.push(2 + " x " + [j] + " = " + [i])
+//     // array.push(2 + "x" + "2 = " +  [i] * 2);
+//     i+=2; 
+//     j++;   
+// }
+// console.log(array);
+ 
+// ***********************************************
+// NAMES *********************************
+// var array = [];
+// var names = ["saad", "ali", "baig", "rayyan", "uzair", "sameer", "saud", "saeed", "saadi"];
+// var i = 0;
+// while(i < 9) {
+//     array.push("my name is " + names[i])
+//     i++;
+// }
+// console.log(array);
+ 
+// **&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&**
+// TABLE WRAPPED IN FUNCTION // ***********************
+// function table(number, second, answer) {
+//     var array = [];
+//     var i = answer;
+//     var j = second;
+//     var k = number;
+//     while(i <= 50) {
+//     array.push(k + " x " + [j] + " = " + [i])
+//     // array.push(2 + "x" + "2 = " +  [i] * 2);
+//     i+=6; 
+//     j++;   
+// }
+// return array;
+// }
+// var result = table(6, 1, 6);
+// console.log(result);
+
+// var arr = [
+//     [1,2], [3,4], [5,6]
+//   ];
+//   for (var i=0; i < arr.length; i++) {
+//     for (var j=0; j < arr[i].length; j++) {
+//       console.log(arr[i][j]);
+//     }
+// }
+
+// var arr = [
+//     [1, 2], [3, 4], [5, 6, 7]
+// ]
+//     var product = 1;
+//     for (var i = 0; i < arr.length; i++) {
+//       for (var j = 0; j < arr[i].length; j++) {
+//         product = product * arr[i][j];
+//       }
+//     }
+// console.log(product);
+
+// var names = ["saad", "ali", "umar", "nabeel", "wasif", "zian", "rayyan", "taha"]
+// var lastNames = ["baig", "mujahid", "meer", "farooq", "ahmed", "romasanta", "mirza", "ali"]
+// var fullName = [];
+// for (i = 0; i < names.length; i++) {
+//   for (j = 0; j < lastNames.length; j++) {
+//     fullName.push(names[i] + " " + lastNames[j])
+//   }
+// }
+// console.log(fullName);
+
+// var items = ["milk", "coke", "doritos"];
+
+// // function priceGenerator(price) {
+//   for (i = 0; i < items.length; i++) {
+//     // items.push("price of " + items[i] + " is " + 10);
+//   }
+// // }
+// // var result = priceGenerator(50);
+// console.log(result);
+
+// function multiply(arr, n) {
+//     var product = arr[0];
+//     for (var i = 1; i <= n; i++) {
+//         product *= arr[i];
+//     }
+//     return product;
+// }
+
+// var result = multiply([5, 6, 7], 2);
+// console.log(result);
+// var array = ["hello", "hi", "bye"];
+// function anxiety(case1) {
+//   for (i = 0; i < 10; i++) {
+//     if (case1 === array[i]) {
+//       console.log('got it');
+//       break;
+//     }
+//     else {
+//       console.log('nothing');
+//       break;
+//     }
+//   }
+// };
+// anxiety("hello");
+
+
+// var shoppingList = ['doritos', 'dew', 'book', 'kfc']
+// var updatedList = [];
+// var total = prompt('What is your total?');
+// var number = parseInt(total);
+// if(number <= 10) {
+//     shoppingList.push('cheese')
+// }
+// else if (number <= 15) {
+//     shoppingList.push('butter')
+// }
+// else {
+//     updatedList.push('nothing added')
+// }
+// console.log(updatedList);
+// console.log(shoppingList);
+
+// RANDOM GENERATOR =============================
+
+// var list = [
+//   "hello", "hi", "shut up", "go away", "nice", "cool"
+// ]
+// function func() {
+//   var result = Math.floor(Math.random() * list.length);
+//   var res = list[result];
+//   console.log(res);
+// }
+// func();
+
+/*
+var a = "55";
+var b = parseInt(a);
+console.log(typeof(a));
+console.log(typeof(b));
+*/
+
+// =========  TERNARY  OPERATOR =====================
+// function ternary(a, b) {
+//   return a === b ? "Equal" : "Not Equal";
+// }
+// ternary(5, 5);
+// console.log(ternary(5, 5));
+
+// =========== MULTIPLE TERNARY OPERATOR =================
+// to be continuted....
---
+++ readme.md
@@ -0,0 +1,15 @@
+// ========================================================= //
+// This technique will give us a whole number between 0 and 19.//
+// Math.floor(Math.random() * 20);                             //
+// ========================================================= //
+
+// ========================================================= //
+// we can generate a random number that falls within a range of two specific numbers. //
+// Here's the formula //
+// Math.floor(Math.random() * (max - min + 1)) + min //
+// ========================================================= //
+
+// ========================================================= //
+
+
+
